e8c2e8ff68f1d130170c0c4d42f6e9519e68e616
Tomasz Bielski <ttbielski@gmail.com>
2022-03-19 12:31:22 +0100

    Add a function that returns computer random choice

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log('Hello')
+function computerPlay() {
+  const shapes = ['rock', 'paper', 'scissors'];
+
+  return shapes[getRandomInt(3)];
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
