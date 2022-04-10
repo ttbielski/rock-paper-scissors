60328fbe9e80df438c4d6f149ab9f5f805aaa229
Tomasz Bielski <ttbielski@gmail.com>
2022-04-10 12:46:12 +0200

    Refactor the game logic

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 const playButtons = document.querySelectorAll('button');
+const scoreDiv = document.querySelector('#score');
+const resultDisplay = document.querySelector('#result');
+let playerPoints = 0;
+let computerPoints = 0;
 
 // Computer choice ///////////////////////
 function computerPlay() {
@@ -15,19 +19,25 @@ function playRound(playerSelection, computerSelection) {
     result = "It's a tie!";
   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
     result = "You won! Rock beats Scissors";
+    playerPoints++;
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
     result = "You won! Paper beats Rock";
+    playerPoints++;
   } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
     result = "You won! Scissors beat Paper";
+    playerPoints++;
   } else if (playerSelection === 'rock' && computerSelection === 'paper') {
     result = "You lost! Paper beats Rock";
+    computerPoints++;
   } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
     result = "You lost! Scissors beat Paper";
+    computerPoints++;
   } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
     result = "You lost! Rock beats Scissors";
+    computerPoints++;
   }
 
-  const resultDisplay = document.querySelector('#result');
+  
   resultDisplay.textContent = result;
 
   return result;
@@ -39,7 +49,14 @@ function game() {
     button.addEventListener('click', e => {
   
       playRound(button.getAttribute('id'), computerPlay());
+      scoreDiv.textContent = `You ${playerPoints} - ${computerPoints} Computer`;
       
+      if (playerPoints === 5) {
+        resultDisplay.textContent = 'You won the game!';
+      } else if (computerPoints === 5) {
+        resultDisplay.textContent = 'Computer won the game!';
+      }
+
     });
   });
   
